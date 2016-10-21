438a87374881ce9481cdb472236d8459aa90e9b4
Liliang <liliang92hg@yeah.net>
2016-10-21 16:38:59 +0800

    创建一个1的文件

--- MyTestForNewFrameWork/MyTestForNewFrameWork/ViewController.m
+++ MyTestForNewFrameWork/MyTestForNewFrameWork/ViewController.m
@@ -24,8 +24,9 @@
     [self initView];
     
 
-//    [self initView];
+    [self initView];
     NSLog(@"测试提交信息是否更改");
+    NSLog(@"测试提交信息是否更改1");
     // Do any additional setup after loading the view, typically from a nib.
 }
 -(void)initView
